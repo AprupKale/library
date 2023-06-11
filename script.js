6086d312dbdc3004aa07829f78659dc0a040f426
AprupKale <aprup.kale@gmail.com>
2023-06-11 12:15:29 +0530

    Enable read toggle button

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function Book(title, author, pages, hasBeenRead) {
     this.pages = pages;
     this.hasBeenRead = hasBeenRead;
 
-    this.info = function() {
+    this.info = function () {
         return this.hasBeenRead
-            ? `${this.title} by ${this.author}, ${this.pages} pages, has been read` 
+            ? `${this.title} by ${this.author}, ${this.pages} pages, has been read`
             : `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
     }
 }
@@ -24,36 +24,50 @@ function addBookToLibrary(book) {
 
 function updateDisplay() {
     const main = document.querySelector('.main');
+    main.innerHTML = '';
+
     for (let i = 0; i < myLibrary.length; i++) {
         let book = myLibrary[i];
 
         const division = document.createElement('div');
         division.classList.add('book')
         division.id = `${book.title}`;
+
         const title = document.createElement('h2');
         title.textContent = `${book.title}`;
+
         const author = document.createElement('h3');
         author.textContent = book.author;
+
         const pages = document.createElement('h4');
-        pages.textContent = `${book.pages} pages`
+        pages.textContent = `${book.pages} pages`;
+
         const readStatus = document.createElement('h4');
-        readStatus.textContent = book.hasBeenRead 
+        readStatus.textContent = book.hasBeenRead
             ? 'Has been read'
             : 'Has not been read';
-        const button = document.createElement('button');
-        button.textContent = 'Delete';
-        button.id = `${book.title}`;
+
+        const remove = document.createElement('button');
+        remove.classList.add('remove');
+        remove.textContent = 'Remove';
+
+        const toggle = document.createElement('button');
+        toggle.classList.add('toggle');
+        toggle.textContent = book.hasBeenRead
+            ? 'Unread'
+            : 'Read';
 
         division.appendChild(title);
         division.appendChild(author);
         division.appendChild(pages);
         division.appendChild(readStatus);
-        division.appendChild(button);
+        division.appendChild(remove);
+        division.appendChild(toggle);
 
         main.appendChild(division);
 
         division
-            .querySelector('button')
+            .querySelector('.remove')
             .addEventListener('click', event => {
                 main.removeChild(division);
 
@@ -62,6 +76,18 @@ function updateDisplay() {
                         myLibrary.splice(i, 1);
                     }
                 }
+            });
+
+        division
+            .querySelector('.toggle')
+            .addEventListener('click', event => {
+                for (let i = 0; i < myLibrary.length; i++) {
+                    if (myLibrary[i].title === `${division.id}`) {
+                        myLibrary[i].hasBeenRead = !myLibrary[i].hasBeenRead;
+                    }
+                }
+
+                updateDisplay();
             });
     }
 }
