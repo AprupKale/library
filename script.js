d1643e0a5d583778113312c1ae3f636cb3fde5ab
AprupKale <aprup.kale@gmail.com>
2023-06-11 11:18:45 +0530

    Enable delete button

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let myLibrary = [];
+let myLibrary = [
+    new Book('Animal Farm', 'George Orwell', 124, true),
+    new Book('War and Peace', 'Leo Tolstoy', 994, false),
+    new Book('Pride and Prejudice', 'Jane Austen', 112, true),
+    new Book('1984', 'George Orwell', 276, false)
+];
 
 function Book(title, author, pages, hasBeenRead) {
     this.title = title;
@@ -16,3 +21,49 @@ function Book(title, author, pages, hasBeenRead) {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+function updateDisplay() {
+    const main = document.querySelector('.main');
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+
+        const division = document.createElement('div');
+        division.classList.add('book')
+        division.id = `${book.title}`;
+        const title = document.createElement('h2');
+        title.textContent = `${book.title}`;
+        const author = document.createElement('h3');
+        author.textContent = book.author;
+        const pages = document.createElement('h4');
+        pages.textContent = `${book.pages} pages`
+        const readStatus = document.createElement('h4');
+        readStatus.textContent = book.hasBeenRead 
+            ? 'Has been read'
+            : 'Has not been read';
+        const button = document.createElement('button');
+        button.textContent = 'Delete';
+        button.id = `${book.title}`;
+
+        division.appendChild(title);
+        division.appendChild(author);
+        division.appendChild(pages);
+        division.appendChild(readStatus);
+        division.appendChild(button);
+
+        main.appendChild(division);
+
+        division
+            .querySelector('button')
+            .addEventListener('click', event => {
+                main.removeChild(division);
+
+                for (let i = 0; i < myLibrary.length; i++) {
+                    if (myLibrary[i].title === `${division.id}`) {
+                        myLibrary.splice(i, 1);
+                    }
+                }
+            });
+    }
+}
+
+updateDisplay();
